d2ef9563d1db5a2c34ff74733af8a595a4106159
abc <abc@abc.com>
2025-09-11 08:45:56 +0800

    add netcore
    Signed-off-by: abc <abc@abc.com>

--- libwrt.sh
+++ libwrt.sh
@@ -3,7 +3,7 @@ git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 
 # 修改默认IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.22.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
