5339899466fb94cec7cdfdf57c7e16b03295aa2b
mojianga <45475118+mojianga@users.noreply.github.com>
2026-05-21 23:14:48 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -6,3 +6,9 @@ sed -i 's/192.168.1.1/192.168.22.1/g' package/base-files/files/bin/config_genera
 
 # TTYD 免登录
 # sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+
+# 替换sing-box
+rm -rf feeds/packages/net/sing-box
+git clone --depth=1 https://github.com/kenzok8/small-package.git /tmp/kenzok8
+mv /tmp/kenzok8/sing-box/ feeds/packages/net/
+
